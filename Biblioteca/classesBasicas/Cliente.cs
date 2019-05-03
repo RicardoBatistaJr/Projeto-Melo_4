eae907dadadf78ef01e03bf49b4f35015fc4466d
Ricardo .Walker <RicardoWalker01@live.com>
2019-05-02 23:41:52 -0300

    init

--- Biblioteca/classesBasicas/Cliente.cs
+++ Biblioteca/classesBasicas/Cliente.cs
@@ -12,10 +12,12 @@ namespace Biblioteca.classesBasicas
         private string nomeCliente;
         private string emailCliente;
         private int telCliente;
+        private string testedealteracao;
 
         public string CpfCliente { get => cpfCliente; set => cpfCliente = value; }
         public string NomeCliente { get => nomeCliente; set => nomeCliente = value; }
         public string EmailCliente { get => emailCliente; set => emailCliente = value; }
         public int TelCliente { get => telCliente; set => telCliente = value; }
+        public string Testedealteracao { get => testedealteracao; set => testedealteracao = value; }
     }
 }
